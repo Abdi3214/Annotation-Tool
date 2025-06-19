325512efb7aa92f44c992ae78be1034547a8e139
Abdi3214 <raboy454>
2025-06-19 16:06:18 +0300

    first commit

--- FrontEnd/BackEnd/models/Translation.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const TranslationSchema = new mongoose.Schema({
-  english:    { type: String, required: true },
-  somali:     { type: String, required: true },
-  // add other fields here…
-}, { collection: 'translations' });
-
-module.exports = mongoose.model('Translation', TranslationSchema);
